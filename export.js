24fa901d514a077bcd86c62734bc756501a9bf1c
Anuchit Chalothorn <anoochit@gmail.com>
2019-10-04 11:16:46 +0700

    init

---
+++ export.js
@@ -0,0 +1,16 @@
+const firestoreService = require('firestore-export-import');
+const serviceAccount = require('./serviceAccountKey.json');
+
+// databaseURL
+const databaseURL = "https://doggygirl.firebaseio.com";
+
+// Initiate Firebase App
+firestoreService.initializeApp(serviceAccount, databaseURL);
+
+// Start exporting your data
+firestoreService
+    .backups(['chats', 'follows', 'images', 'users'])
+    .then(collections => {
+        // You can do whatever you want with collections
+        console.log(JSON.stringify(collections));
+    })
